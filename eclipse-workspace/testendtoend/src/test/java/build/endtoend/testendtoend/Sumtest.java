7448dd411a423d00365a78aa6d65c509272e2707
singi123 <sandhya.singir@gmail.com>
2021-03-23 15:03:36 -0400

    initial commit

---
+++ eclipse-workspace/testendtoend/src/test/java/build/endtoend/testendtoend/Sumtest.java
@@ -0,0 +1,11 @@
+package build.endtoend.testendtoend;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Sumtest {
+  @Test
+  public void numbSumtest() {
+	  Assert.assertEquals(7, 7);
+  }
+}
